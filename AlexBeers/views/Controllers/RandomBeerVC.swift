6a9c445ad508f0d34ed67f27cf68cb15ce92b649
deidg <64797316+deidg@users.noreply.github.com>
2024-01-31 17:40:48 +0300

    added beerList VC with funcionality

---
+++ AlexBeers/views/Controllers/RandomBeerVC.swift
@@ -0,0 +1,33 @@
+//
+//  RandomBeerVC.swift
+//  AlexBeerShop
+//
+//  Created by Alex on 17.01.2024.
+//
+
+import UIKit
+
+
+class RandomBeerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupVC()
+        setupUI()
+        
+        
+    }
+
+    private func setupVC() {
+        view.backgroundColor = .blue
+        
+        title = "Random"
+        
+    }
+    
+    private func setupUI() {
+    
+    }
+    
+   
+}
---
+++ AlexBeers/views/Controllers/SearchVC.swift
@@ -0,0 +1,34 @@
+//
+//  SearchVC.swift
+//  AlexBeerShop
+//
+//  Created by Alex on 17.01.2024.
+//
+
+import UIKit
+
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupVC()
+        setupUI()
+        
+        
+    }
+
+    private func setupVC() {
+        view.backgroundColor = .cyan
+        
+        title = "Search ID"
+        
+    }
+    
+    private func setupUI() {
+    
+    }
+    
+   
+    
+}
